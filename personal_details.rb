4bb4c9fa10a582ded1b439eb9b841c1cf97acc28
lyle Sanders <lylesanders66@yahoo.com>
2016-09-20 14:27:27 -0400

    input for age

--- personal_details.rb
+++ personal_details.rb
@@ -3,3 +3,13 @@ require 'sinatra'	# gem install sinatra
 get '/' do # posts to screen
 	erb :get_name
 end
+
+post '/name' do
+	name = params[:user_name]
+    redirect 'age?users_name' + name
+end
+
+get '/age' do
+	name = params[:user_name]
+	erb :get_age, :locals=>{:name => name}
+end
