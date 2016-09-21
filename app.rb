2e57e3d64d198d20b4184fe9f726ad4806c49f2f
lyle Sanders <lylesanders66@yahoo.com>
2016-09-21 09:07:31 -0400

    code passing users_name into get_age

--- app.rb
+++ app.rb
@@ -6,11 +6,11 @@ get '/' do
 end
 
 post '/users_name' do 
-	name = params[:user_name]
-	redirect '/age?users_name' + name 
+	name = params[:user_name]  # user_name matches get_name <input type ="text" name ="user_name">
+	redirect '/age?users_name=' + name # users_name matches get_name <form metopd "post" action"/users_name>
 end
 
 get '/age' do 
-	name = params[:user_name]
+	name = params[:users_name]
 	erb :get_age , :locals =>{:name => name}
 end
