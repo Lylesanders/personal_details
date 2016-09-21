bd0e7fff24bfb0af86adba575dfe70b5848c3894
lyle Sanders <lylesanders66@yahoo.com>
2016-09-21 10:47:49 -0400

    add get_favs

--- app.rb
+++ app.rb
@@ -14,3 +14,18 @@ get '/age' do
 	name = params[:users_name]
 	erb :get_age , :locals =>{:name => name}
 end
+ 
+post '/age' do 
+ 	age = params[:age]
+ 	name = params[:retrieved_name]
+ 	redirect '/fav_nums?name=' + name +"&age=" +age
+end
+
+get '/fav_nums' do
+	name = params[:name]
+	age = params[:age]
+	erb :get_favs , :locals =>{:name => name, :age => age}
+end
+post '/fav_nums' do
+  "Hello World"
+end
